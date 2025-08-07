5b49ff27f8a46dc378af70a7cacefec3e5d56f88
sebas088 <sebastianbecerra088@gmail.com>
2025-08-07 15:26:09 -0700

    Correcion de bug al cerrar formulario y cambio de gama de colores de toda la pagina

--- src/components/FloatingButton/FloatingButton.tsx
+++ src/components/FloatingButton/FloatingButton.tsx
@@ -1,13 +1,17 @@
+import { forwardRef } from 'react';
 import './FloatingButton.css';
 
 interface FloatingButtonProps {
   onClick: () => void;
 }
 
-const FloatingButton = ({ onClick }: FloatingButtonProps) => (
-  <button className="floatingButton" onClick={onClick}>
-    Contact
-  </button>
+const FloatingButton = forwardRef<HTMLButtonElement, FloatingButtonProps>(({ onClick }, ref) => {
+  return (
+    <button className="floatingButton" onClick={onClick} ref={ref}>
+      Contact
+    </button>
+  );
+}
 );
 
 export default FloatingButton;
--- src/components/Pages/Contact/Contact.tsx
+++ src/components/Pages/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import Form from "./Form/Form"; // Formulario de contacto
 import FloatingButton from "../../FloatingButton/FloatingButton";
 import './Contact.css';
@@ -6,15 +6,41 @@ import './Contact.css';
 
 const Contact = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const panelRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   const toggleForm = () => setIsOpen(!isOpen);
 
+  useEffect(() => {
+    const handelClickOutside = (event: MouseEvent) => {
+      const target = event.target as Node;
+      if (
+        isOpen &&
+        panelRef.current &&
+        !panelRef.current.contains(target) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(target)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handelClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handelClickOutside);
+    };
+  }, [isOpen]);
+
   return (
     <div>
-      <FloatingButton onClick={toggleForm} />
+      <FloatingButton onClick={toggleForm} ref={buttonRef}/>
 
       {/* Panel flotante */}
-      <div className={`contactPanel ${isOpen ? 'open' : ''}`}>
+      <div 
+        className={`contactPanel ${isOpen ? 'open' : ''}`}
+        ref={panelRef}
+      >
         <Form />
       </div>
     </div>
